6414c8595ba7470a34047c663a0e4dc945a1a4dd
Guruprasad Venkataraghavan <vguruprasad91@gmail.com>
2017-01-07 00:52:28 -0500

    Added the code for the backward propagation

--- _3._background_propagation.py
+++ _3._background_propagation.py
@@ -1,1 +1,22 @@
 
+# Create a class Neuron
+class Neuron:
+  
+  # instantiate all the values for the neuron class
+  def __init__(self,inbound_neurons=[]):
+    
+    # Assign the values for the inbound_neurons
+    self.inbound_neurons = inbound_neurons
+    
+    # Assign the values for the outbound_neurons
+    self.outbound_neurons = []
+    
+    # Assign the values for the output calculated values
+    self.value = None
+    
+    # Add this node as an outbound node on its inputs
+    for n in self.inbound_neurons:
+      n.outbound_neurons.append(self)
+    
+  def backward(self):
+    raise NotImplemented
