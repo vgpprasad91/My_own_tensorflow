cc630ca45b542bb46c30f671731a84d568442ce0
Guruprasad Venkataraghavan <vguruprasad91@gmail.com>
2017-01-07 01:09:17 -0500

    Added code for the input neurons

--- neuron_subclasses/_3._input_neuron.py
+++ neuron_subclasses/_3._input_neuron.py
@@ -1,1 +1,15 @@
 
+# Create a class Input Neuron
+class Input(Neuron):
+  
+  # Initialize all the values
+  def __init__(self):
+    
+    # An input neuron has no inbound_neurons
+    Neuron.__init__(self)
+    
+    # Create a function for forward propagation from input neurons
+    def forward_propagation(self,value=None):
+      # Overwrite the calculated value if one is passed in
+      if value is Not None:
+        self.value = value
