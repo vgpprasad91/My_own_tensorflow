d129b0b6b9cd223dc60ec88c9e7dab1b15c89be4
Guruprasad Venkataraghavan <vguruprasad91@gmail.com>
2017-01-07 00:35:07 -0500

    Add code for forward propagation

--- _2._forward_propagation.py
+++ _2._forward_propagation.py
@@ -1,1 +1,20 @@
-
+class neuron:
+  
+  #initialite all the values using __init__ function
+  def __init__(self,inbound_neurons=[]):
+    
+    # Add a code to assign inbound_neurons
+    self.inbound_neurons = inbound_neurons
+    
+    # Add a code to assign outbound_neurons
+    self.outbound_neurons = []
+    
+    # Add this node as an outbound node on its inputs
+    for n in self.inbound_neurons:
+      n.outbound_neurons.append(self)
+    
+    # Add a code to assign a calculated value
+    self.value = None
+    
+    def forward(self):
+      raise NotImplemented
