aec757c3c600c066745d633a18ba7acb59b38c3a
Guruprasad Venkataraghavan <vguruprasad91@gmail.com>
2017-01-07 01:43:01 -0500

    Update _2._calculation_with_neuron_subclasses.py

--- neuron_subclasses/_2._calculation_with_neuron_subclasses.py
+++ neuron_subclasses/_2._calculation_with_neuron_subclasses.py
@@ -1,1 +1,10 @@
 
+# create a neuron subclass to perform addition
+class Add(Neuron):
+  
+  # Instantiate all the values
+  def __init__(self,x,y):
+    Neuron.__init__(self, [x,y])
+    
+  # Create a code to perform forward propagation
+  
