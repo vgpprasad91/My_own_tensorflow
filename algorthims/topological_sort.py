d0f8a00bb4523eb62c92dad2d0d6430491679916
Guruprasad Venkataraghavan <vguruprasad91@gmail.com>
2017-01-07 01:53:20 -0500

    Update topological_sort.py

--- algorthims/topological_sort.py
+++ algorthims/topological_sort.py
@@ -1,1 +1,11 @@
 
+def topological_sort(input_nodes_dict):
+  """
+  Sort generic nodes in topological sort using Khan's algorithm
+  'input_nodes_dict': A dictionary where the key is a 'Input' node and the value is the respective value feed to that node
+  Returns a list of sorted node
+  """
+  
+  input_nodes = [n for n in input_nodes_dict]
+  
+  
