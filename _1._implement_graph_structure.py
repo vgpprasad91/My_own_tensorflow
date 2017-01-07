568e4c5b7ebee1bbefd3851e73312a956c05a6c5
Guruprasad Venkataraghavan <vguruprasad91@gmail.com>
2017-01-07 00:12:07 -0500

    update code for inbound and outbound neurons

--- _1._implement_graph_structure.py
+++ _1._implement_graph_structure.py
@@ -1,4 +1,14 @@
 
 class Neuron:
-  def __init__(self):
+  def __init__(self, inbound_neurons = []):
     # properties will go here
+    
+    # Neuron from which this neuron receives values
+    self.inbound_neurons = inbound_neurons
+    
+    # Neuron to which this neuron passes values
+    self.outbound_neurons = []
+    
+    # For each inbound neuron here, add this neuron as an outbound neuron there
+    for n in self.inbound_neurons:
+      n.outbound_neurons.append(self)
