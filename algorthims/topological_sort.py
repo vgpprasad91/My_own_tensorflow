3b8962bf0ac62b251fd9f199019ac07037a4e19a
Guruprasad Venkataraghavan <vguruprasad91@gmail.com>
2017-01-07 11:44:03 -0500

    Update topological_sort.py

--- algorthims/topological_sort.py
+++ algorthims/topological_sort.py
@@ -6,6 +6,24 @@ def topological_sort(input_nodes_dict):
   Returns a list of sorted node
   """
   
-  input_nodes = [n for n in input_nodes_dict]
+  input_nodes = [n for n in input_nodes_dict.keys()]
+  
+  graph = {}
+  nodes= [n for n in input_nodes]
+  while len(nodes) > 0:
+    n = nodes.pop(0)
+    if n not in graph:
+      graph[n] = {'in': set(), 'out': set()}
+    for m in n.outbound_nodes:
+      if m not in graph:
+        graph[m] = {'in':set(), 'out':set()}
+      graph[n]['out'].add(m)
+      graph[m]['in'].add(n)
+      nodes.append(m)
+  L = []
+  S = set(input_nodes)
+  while len(S) > 0:
+    n = S.pop(0)
+    if isinstance(
   
   
